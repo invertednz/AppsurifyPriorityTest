f04709af360cbf269018c9e60e1503cdd7fdf0f4
invertednz <jamesfarrier@gmail.com>
2019-11-09 20:32:30 +0000

    commit8

--- src/Area2/File1Area2.java
+++ src/Area2/File1Area2.java
@@ -4,6 +4,6 @@ package Area2;
  * Created by james on 13/06/2018.
  */
 public class File1Area2 {
-	int i=0;
+	int i=1;
 
 }
--- src/Area3/File1Area3.java
+++ src/Area3/File1Area3.java
@@ -4,6 +4,6 @@ package Area3;
  * Created by james on 13/06/2018.
  */
 public class File1Area3 {
-	int i=0;
+	int i=10;
 
 }
